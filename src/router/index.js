de1de799778d38e0be697bbc0eeac2a723e0c5c5
蒋建明 <jiangjianming@bmkp.cn>
2018-04-28 18:40:01 +0800

    add head.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import App from '../App'
+const home = resolve => require(['../page/home/home'], resolve)
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'app',
+      component: App,
+      children: [{
+        path: '',
+        component: home
+      }]
     }
   ]
 })
