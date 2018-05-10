e32bf6115d26966cf7768cb982480531e7716eda
蒋建明 <jiangjianming@bmkp.cn>
2018-05-10 18:58:03 +0800

    add fade effect for close activities

--- src/page/shop/shop.vue
+++ src/page/shop/shop.vue
@@ -8,33 +8,63 @@
           </svg>
         </nav>
         <header class="shop_detail_header" ref="shopheader" :style="{zIndex: showActivities?'14':'10'}">
-        <img :src="imgBaseUrl + shopDetailData.image_path" class="header_cover_img">
-        <section class="description_header">
-          <router-link to="/shop/shopDetail" class="description_top">
-            <section class="description_left">
-              <img :src="imgBaseUrl + shopDetailData.image_path">
+          <img :src="imgBaseUrl + shopDetailData.image_path" class="header_cover_img">
+          <section class="description_header">
+            <router-link to="/shop/shopDetail" class="description_top">
+              <section class="description_left">
+                <img :src="imgBaseUrl + shopDetailData.image_path">
+              </section>
+              <section class="description_right">
+                <h4 class="description_title ellipsis">{{shopDetailData.name}}</h4>
+                <p class="description_text">商家配送/{{shopDetailData.order_lead_time}}分钟送达/配送费￥{{shopDetailData.float_delivery_fee}}</p>
+                <p class="description_promotion ellipsis">公告：{{promotionsInfo}}</p>
+              </section>
+              <svg width="14" height="14" xmlns="http://www.w3.org/2000/svg" version="1.1" class="description_arrow" >
+                <path d="M0 0 L8 7 L0 14"  stroke="#fff" stroke-width="1" fill="none"/>
+              </svg>
+            </router-link>
+            <footer class="description_footer" v-if="shopDetailData.activities.length">
+              <p class="ellipsis">
+                <span class="tip_icon" :style="{backgroundColor:'#' + shopDetailData.activities[0].icon_color,borderColor:'#' + shopDetailData.activities[0].icon_color}">{{shopDetailData.activities[0].icon_name}}</span>
+                <span>{{shopDetailData.activities[0].description}}(APP专享)</span>
+              </p>
+              <p>{{shopDetail.activities.length}}个活动</p>
+              <svg class="footer_arrow">
+                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-left"></use>
+              </svg>
+            </footer>
+          </section>
+        </header>
+        <transition name="fade">
+          <section class="activities_details" v-if="showActivities">
+            <h2 class="activities_shoptitle">{{shopDetailData.name}}</h2>
+            <div class="activities_ratingstar">
+              <rating-star :rating="shopDetailData.rating"></rating-star>
+            </div>
+            <section class="activities_list">
+              <header class="activities_title_style">
+                <span>优惠信息</span>
+              </header>
+              <ul>
+                <li v-for="item in shopDetailData.activities" :key="item.id">
+                  <span class="activities_icon" :style="{backgroundColor:'#' + item.icon_color, borderColor: '#' + item.icon_color}">{{item.icon_name}}</span>
+                  <span>{{item.description}}(App专享)</span>
+                </li>
+              </ul>
             </section>
-            <section class="description_right">
-              <h4 class="description_title ellipsis">{{shopDetailData.name}}</h4>
-              <p class="description_text">商家配送/{{shopDetailData.order_lead_time}}分钟送达/配送费￥{{shopDetailData.float_delivery_fee}}</p>
-              <p class="description_promotion ellipsis">公告：{{promotionsInfo}}</p>
+            <section class="activities_shopinfo">
+              <header class="activities_title_style">
+                <span>商家公告</span>
+              </header>
+              <p>{{promotionsInfo}}</p>
             </section>
-            <svg width="14" height="14" xmlns="http://www.w3.org/2000/svg" version="1.1" class="description_arrow" >
-              <path d="M0 0 L8 7 L0 14"  stroke="#fff" stroke-width="1" fill="none"/>
+            <svg width="60" height="60" class="close_activities" @click.stop="showActivitiesFun">
+              <circle cx="30" cy="30" r="25" stroke="#555" stroke-width="1" fill="none"/>
+              <line x1="22" y1="38" x2="38" y2="22" style="stroke:#999;stroke-width:2"/>
+              <line x1="22" y1="22" x2="38" y2="38" style="stroke:#999;stroke-width:2"/>
             </svg>
-          </router-link>
-          <footer class="description_footer" v-if="shopDetailData.activities.length">
-            <p class="ellipsis">
-              <span class="tip_icon" :style="{backgroundColor:'#' + shopDetailData.activities[0].icon_color,borderColor:'#' + shopDetailData.activities[0].icon_color}">{{shopDetailData.activities[0].icon_name}}</span>
-              <span>{{shopDetailData.activities[0].description}}(APP专享)</span>
-            </p>
-            <p>{{shopDetail.activities.length}}个活动</p>
-            <svg class="footer_arrow">
-              <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-left"></use>
-            </svg>
-          </footer>
-        </section>
-      </header>
+          </section>
+        </transition>
       </section>
       <transition name="fade"></transition>
       <section class="animation_opactiy shop_back_svg_container" v-if="showLoading">
@@ -61,7 +91,7 @@ export default {
       showLoading: true, // 显示加载动画
       changeShowType: 'food', // 切换显示商品或者评价
       shopDetailData: null, // 店铺详情
-      showActivities: false, // 是否显示活动详情
+      showActivities: true, // 是否显示活动详情
       menuList: [], // 食品列表
       menuIndex: 0, // 已选菜单索引值，默认0
       menuIndexChange: true, // 解决选中index时，scroll监听事件重复判断设置index的bug
@@ -139,6 +169,10 @@ export default {
     },
     goback () {
       this.$router.go(-1)
+    },
+    // 控制活动详情页的显示隐藏
+    showActivitiesFun () {
+      this.showActivities = !this.showActivities
     }
   },
   components: {
